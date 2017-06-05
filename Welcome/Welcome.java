d7e2a09e00d9f1eebf52f5f4bf251ee20146539c
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-05 11:03:22 +0100

    Welcome Class Prints A Single Line With Single Statement

---
+++ Welcome/Welcome.java
@@ -0,0 +1,12 @@
+// My first Java Program
+// Welcome to Java Programming Fundamentals
+
+public class Welcome
+{
+
+// main method begins execution of Java application
+  public static void main( String[] args )
+    {
+      System.out.println( "Welcome to Java Programming Fundamentals!" );
+    } // end method main
+} // end class Welcome1
