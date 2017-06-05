8c6c0538e2ebbc1e48dc2138ee625d3629007150
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-05 14:55:14 +0100

    Self review Exerise 17 adds average and equality to Arithmetic operations

--- Arithmetic/Arithmetic.java
+++ Arithmetic/Arithmetic.java
@@ -12,7 +12,7 @@ public class Arithmetic
     Scanner input = new Scanner ( System.in );
 
     int first, second;
-    int sum , product, difference, quotient;
+    int sum , product, difference, quotient, average, small, large;
 
     System.out.print("Enter First Integer : ");
     first = input.nextInt();
@@ -24,11 +24,26 @@ public class Arithmetic
     product = first * second;
     difference = first - second;
     quotient = first / second;
+    average = ( first + second ) / 2;
+
+    if (first <= second)
+      {
+        small = first;
+        large = second;
+      }
+    else
+      {
+        small = second;
+        large = first;
+      }
 
 
     System.out.printf("Sum of %d and %d = %d\n", first, second, sum );
     System.out.printf("Product of %d and %d = %d\n", first, second, product);
     System.out.printf("Difference between %d and %d = %d\n", first, second, difference);
     System.out.printf("Quotient of %d and %d = %d\n", first, second, quotient);
+    System.out.printf("Average of %d and %d = %d\n", first, second, average);
+    System.out.printf("Smaller of %d and %d = %d\n", first, second, small);
+    System.out.printf("Larger of %d and %d = %d\n", first, second, large );
   }
 }
