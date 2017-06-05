dbe47e213289eb5a81cad79038f55fc6ca268ffe
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-05 11:18:04 +0100

    Welcome program Uses Formatted Printing to print each word on a separate line

--- Welcome/Welcome.java
+++ Welcome/Welcome.java
@@ -7,7 +7,6 @@ public class Welcome
 // main method begins execution of Java application
   public static void main( String[] args )
     {
-      System.out.print("Welcome To \n");
-      System.out.println( "Java Programming \nFundamentals!" );
+      System.out.printf("%s\n%s\n%s\n%s\n%s", "Welcome", "To", "Java", "Programming", "Fundamentals!");
     } // end method main
 } // end class Welcome1
