8d4ad2a2dc0adc17896b747214a006b9641e28ae
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-06 13:18:55 +0100

    Self Review 2.27 prints a checkboard of asterisks

---
+++ Exercises/Self_Review_2_27.java
@@ -0,0 +1,18 @@
+//Self_Review_2_27.java
+// Program print a checkerboard of asterisks
+
+public class Self_Review_2_27
+{
+  public static void main ( String[] args )
+  {
+    System.out.println("* * * * * * * *");
+    System.out.println(" * * * * * * * *");
+    System.out.println("* * * * * * * *");
+    System.out.println(" * * * * * * * *");
+    System.out.println("* * * * * * * *");
+    System.out.println(" * * * * * * * *");
+    System.out.println("* * * * * * * *");
+    System.out.println(" * * * * * * * *");
+
+  }
+}
