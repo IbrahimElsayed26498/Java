88fe0a2fc2a564bc03028d31673af506025dc69b
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-08 11:56:28 +0100

    Displays multiple lines of text in Dialog box

---
+++ Chapter3/Dialog/Dialog.java
@@ -0,0 +1,12 @@
+// Dialog.java
+
+import javax.swing.JOptionPane;
+
+public class Dialog
+{
+  public static void main (String[] args )
+  {
+    // Display a lines of texts within a box
+    JOptionPane.showMessageDialog(null, " Welcome To Java \n The Beautiful Language ");
+  }
+}
