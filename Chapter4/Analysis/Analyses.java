1d00120ea6ce20d1f5ff11da7f47cf8ff39f57db
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-09 16:14:35 +0100

    Analysis class works

---
+++ Chapter4/Analysis/Analyses.java
@@ -0,0 +1,38 @@
+// Analyses.java
+
+import java.util.Scanner;
+
+public class Analyses
+{
+  public static void main(String[] args)
+  {
+    Scanner input = new Scanner(System.in);
+
+    int passes = 0;
+    int failures = 0;
+    int studentCounter = 1;
+    int result;
+
+    while ( studentCounter <= 10 )
+    {
+      // prompt user for input and obtain value from user
+      System.out.print( "Enter result (1 = pass, 2 = fail): " );
+      result = input.nextInt();
+
+      if (result == 1)
+          passes++;
+      else
+          failures++;
+
+
+      // increment studentCounter so loop eventually terminates
+      studentCounter = studentCounter + 1;
+    } // end while
+
+    // termination phase; prepare and display results
+    System.out.printf( "Passed: %d\nFailed: %d\n", passes, failures );
+
+    if ( passes > 8 )
+        System.out.println( "Bonus to instructor!" );
+  }
+}
