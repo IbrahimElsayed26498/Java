40b32ef068eaa088a09ef94dba76818b9d46c24e
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-13 12:40:59 +0100

    Figure 5.2 Counter-controlled repition with for statement

---
+++ Chapter5/ForCounter/ForCounter.java
@@ -0,0 +1,12 @@
+public class ForCounter
+{
+  public static void main(String[] args)
+  {
+    int counter;
+
+    for ( counter = 1; counter <= 10; counter++ )
+        System.out.printf("%d ", counter );
+
+    System.out.println();
+  }
+}
