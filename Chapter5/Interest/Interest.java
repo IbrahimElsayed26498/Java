c8d3339f0fb3f31a830954ab65e4a716105785b6
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-15 10:27:36 +0100

    Exercise 5.14 Computes Both Simple Interest And Compound Interest

--- Chapter5/Interest/Interest.java
+++ Chapter5/Interest/Interest.java
@@ -1,23 +1,47 @@
 // Interest.java
 // Compound Interest Calculations With For Loops
 
+import java.util.Scanner;
+
 public class Interest
 {
   public static void main(String[] args)
   {
+    Scanner input = new Scanner ( System.in );
+
     double amount;
     double principal = 1000.0;
     double rate = 0.05;
 
-    System.out.printf("%s\t%s\n", " Year ", " Amount On Deposit " );
+    System.out.printf("To Find Simple Interest, Enter 1\nTo Find Compound Interest, Enter 2\n");
+    int choice = input.nextInt();
 
-    // calculate amount on deposit for each of ten years
-    for ( int year = 1; year <= 10; year++ )
+    switch( choice )
     {
-      // calculate new amount for specified year
-      amount = principal * Math.pow( 1.0 + rate, year );
-      // display the year and the amount
-      System.out.printf( "%4d%,20.2f\n", year, amount );
-    } // end for
+      case 1:
+        {
+          System.out.printf("Simple Interest Is %.2f", principal * rate * 10 );
+        }
+        break;
+
+      case 2:
+        {
+          System.out.printf("%s\t%s\n", " Year ", " Amount On Deposit " );
+
+          // calculate amount on deposit for each of ten years
+          for ( int year = 1; year <= 10; year++ )
+          {
+            // calculate new amount for specified year
+            amount = principal * Math.pow( 1.0 + rate, year );
+            // display the year and the amount
+            System.out.printf( "%4d%,20.2f\n", year, amount );
+          } // end for
+        }
+        break;
+
+      default:
+        System.out.printf("You need to enter either 1 or 2");
+        break;
+    }
   }
 }
