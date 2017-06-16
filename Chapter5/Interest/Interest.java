70097e71947dd2af7a3369b9649f2cd1a702bfd4
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-16 09:43:22 +0100

    Exercise 5.18 Breaks Simple And Compound Interest Results Into Dollars And Cents

--- Chapter5/Interest/Interest.java
+++ Chapter5/Interest/Interest.java
@@ -1,5 +1,6 @@
 // Interest.java
 // Compound Interest Calculations With For Loops
+// Exercise 5.14 And 5.18 Solved Here
 
 import java.util.Scanner;
 
@@ -10,7 +11,7 @@ public class Interest
     Scanner input = new Scanner ( System.in );
 
     double amount;
-    double principal = 1000.0;
+    double principal = 100000.0; // In cents
     double rate = 0.05;
 
     System.out.printf("To Find Simple Interest, Enter 1\nTo Find Compound Interest, Enter 2\n");
@@ -20,13 +21,13 @@ public class Interest
     {
       case 1:
         {
-          System.out.printf("Simple Interest Is %.2f", principal * rate * 10 );
+          System.out.printf("Simple Interest Is $%.2f", principal * rate * 10 / 100 );
         }
         break;
 
       case 2:
         {
-          System.out.printf("%s\t%s\n", " Year ", " Amount On Deposit " );
+          System.out.printf("%s\t\t%s\n", " Year ", "Amount On Deposit " );
 
           // calculate amount on deposit for each of ten years
           for ( int year = 1; year <= 10; year++ )
@@ -34,7 +35,7 @@ public class Interest
             // calculate new amount for specified year
             amount = principal * Math.pow( 1.0 + rate, year );
             // display the year and the amount
-            System.out.printf( "%4d%,20.2f\n", year, amount );
+            System.out.printf( " %4d\t\t$%.0f,¢%.0f \n", year, amount / 100, amount % 100 );
           } // end for
         }
         break;
