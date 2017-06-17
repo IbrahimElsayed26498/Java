cc1a44151633c15412376478bb194e8ee4ce9d3e
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-17 12:40:01 +0100

    Alternating Random Colors Now Get Printed For Fig. 6.13

--- Chapter6/BullEye/BullEye.java
+++ Chapter6/BullEye/BullEye.java
@@ -9,24 +9,30 @@ public class BullEye extends JPanel
   {
     super.paintComponent( g );
 
+    Random randomNumberOne = new Random();
+    Color firstColor = new Color( randomNumberOne.nextInt(255), randomNumberOne.nextInt(255), randomNumberOne.nextInt(255));
+
+    Random randomNumberTwo = new Random();
+    Color secondColor = new Color( randomNumberTwo.nextInt(255), randomNumberTwo.nextInt(255), randomNumberTwo.nextInt(255));
+
     //draw the first circle
-    g.setColor( Color.DARK_GRAY );
+    g.setColor( firstColor );
     g.fillOval( 10, 10, 200, 200 );
 
     // draw the second circle
-    g.setColor( Color.GREEN );
+    g.setColor( secondColor );
     g.fillOval( 30, 30, 160, 160 );
 
     // draw the third circle
-    g.setColor( Color.BLUE );
+    g.setColor( firstColor );
     g.fillOval( 50, 50, 120, 120 );
 
     // draw the fourth circle
-    g.setColor( Color.CYAN );
+    g.setColor( secondColor );
     g.fillOval( 65, 65, 90, 90 );
 
     // draw the fifth circle
-    g.setColor( Color.MAGENTA );
+    g.setColor( firstColor );
     g.fillOval( 80, 80, 60, 60 );
   }
 }
--- Chapter6/BullEye/BullEyeTest.java
+++ Chapter6/BullEye/BullEyeTest.java
@@ -6,19 +6,17 @@ public class BullEyeTest
 {
   public static void main( String[] args )
   {
-    BullEye panel1 = new BullEye();
+    BullEye panel = new BullEye();
     JFrame application1 = new JFrame();
-
-    BullEye panel2 = new BullEye();
-    JFrame application2 = new JFrame();
-
     application1.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-    application1.add( panel1 );
+    application1.add( panel );
     application1.setSize( 230, 250 );
     application1.setVisible( true );
 
+    BullEye panel1 = new BullEye();
+    JFrame application2 = new JFrame();
     application2.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-    application2.add( panel2 );
+    application2.add( panel1 );
     application2.setSize( 230, 250 );
     application2.setVisible( true );
   }
