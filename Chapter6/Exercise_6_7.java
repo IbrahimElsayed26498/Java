297d92655fd587c298c21d3d30c293b55757f3be
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-17 14:07:26 +0100

    Exercise 6.7 Tests Math Library Functions

---
+++ Chapter6/Exercise_6_7.java
@@ -0,0 +1,14 @@
+// Exercise_6_7.java
+
+public class Exercise_6_7
+{
+  public static void main( String[] args )
+  {
+    System.out.printf( "Absolute Value Of 7.5 Is %.1f\n", Math.abs( 7.5 ) );
+    System.out.printf( "Floor Of 7.5 Is %.1f\n", Math.floor( 7.5 ) );
+    System.out.printf( "Absolute Value Of 0.0 Is %.1f\n", Math.abs( 0.0 ) );
+    System.out.printf( "Absolute Value Of -6.4 Is %.1f\n", Math.abs( -6.4 ) );
+    System.out.printf( "Ceiling Of -6.4 Is %.1f\n", Math.ceil( -6.4 ) );
+    System.out.printf( "Ceiling Of X Is %.1f\n", Math.ceil( -8.0 + Math.floor( -5.5 ) ) );
+  }
+}
