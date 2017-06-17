c0cb280c89eb5aa874add79d8885eaa402b0ae05
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-17 14:52:10 +0100

    Exercise 6.8 Finds Parking Charges For Several Customers

--- Chapter6/Parking/Parking.java
+++ Chapter6/Parking/Parking.java
@@ -6,23 +6,36 @@ public class Parking
 {
   public static void main(String[] args )
   {
-    System.out.println("Program Finds The Parking Charges For a vehicle");
+    System.out.println("Program Finds The Parking Charges For Customers' Cars");
 
     Scanner input = new Scanner (System.in);
 
-    double hours;
-    System.out.print("Enter The Number of Hours ( 0.0 to 24.0) Your Car Was Parked Here : ");
-    hours = input.nextDouble();
+    double[] hours = new double[5];
 
+    for ( int i = 0; i < 5; i++)
+    {
+      System.out.printf("Enter The Number of Hours ( 0.0 to 24.0) Car %d Was Parked Here : ", i + 1 );
+      hours[i] = input.nextDouble();
+    }
+
+    System.out.printf("Car\t\tParking Charge\n");
+    for ( int j = 0; j < 5; j++)
+    {
+      System.out.printf("Car %d\t\t  %.2f\n", j, findCharge(hours[j]));
+    }
+  }
+
+  public static double findCharge( double hh )
+  {
     double charge = 0.00;
 
-    if (hours <= 3.00)
+    if (hh <= 3.00)
       charge = 2.00;
     else
     {
-      charge = Math.min( 2.00 + 0.50 * ( hours - 3.00), 10.00);
+      charge = Math.min( 2.00 + 0.50 * ( hh - 3.00), 10.00);
     }
 
-    System.out.printf("Your parking Charge Is %.2f", charge);
+    return charge;
   }
 }
