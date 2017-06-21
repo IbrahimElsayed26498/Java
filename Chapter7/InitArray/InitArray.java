bdaf2126d8aa7dd95aacbf7972791cdf6f75f816
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-06-21 15:30:14 +0100

    Fig. 7.17 Initializing two-dimensional arrays.

--- Chapter7/InitArray/InitArray.java
+++ Chapter7/InitArray/InitArray.java
@@ -5,6 +5,10 @@ public class InitArray
   public static void main( String[] args )
   {
     //int[] array = new int[10];
+
+    int[][] array1 = {{ 1, 2, 3}, {4, 5, 6 }};
+    int[][] array2 = { {1, 2}, { 3 }, {4, 5, 6} };
+
     final int ARRAY_LENGTH = 10;
     int[] array = new int[ARRAY_LENGTH];
 
@@ -15,5 +19,16 @@ public class InitArray
 
     for ( int counter = 0; counter < array.length; counter++ )
          System.out.printf( "%5d%8d\n", counter, array[ counter ] );
+  }
+
+  public static void outputArray( int[][] array )
+  {
+    for ( int row = 0; row < array.length; row++)
+    {
+      for ( int column = 0; column < array[row].length; column++)
+          System.out.printf("%d ", array[ row ][ column ]);
+
+      System.out.println();
+    }
   }
 }
