9a2737c1de6ccb25c4e2c62994949dbb02520bb3
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-07-04 11:10:27 +0100

    Just improved upon this

--- Chapter7/Turtle/Turtle.java
+++ Chapter7/Turtle/Turtle.java
@@ -6,7 +6,8 @@ import java.util.Random;
 public class Turtle
 {
   private enum Status { DOWN, UP };
-  private enum Direction { RIGHT, LEFT };
+  private enum Direction { NORTH, WEST, SOUTH, EAST };
+  private enum Turn { LEFT, RIGHT };
 
   public static void main( String[] args )
   {
@@ -17,10 +18,12 @@ public class Turtle
         for ( int j = 0; j < 20; j++ )
             floor[i][j] = 0;
 
-    int[] commands = { 2, 5, 12, 3, 5, 12, 3, 5, 12, 3, 5, 12, 1, 6, 9 };
+    int[] commands = { 1, 2, 5, 12, 5, 12, 4, 5, 12, 4, 5, 12, 4, 5, 12, 6, 1 };
 
-    int now = commands[0];
-    int next = commands[1];
+    int currentNow = 0, currentNext = 1;
+
+    int now = commands[currentNow];
+    int next = commands[currentNext];
 
     int currentX = 0, currentY = 0;
 
@@ -29,55 +32,108 @@ public class Turtle
     int count = 0, c = 0;
 
     Status status = Status.UP;
-    Direction facing = Direction.RIGHT;
+    Direction facing = Direction.SOUTH;
+    Direction currentDirection;
 
-    for ( int command : commands )
+    for ( int command = 0; command < commands.length; command++ )
     {
-      switch( command )
+      switch( commands[command] )
       {
         case 1:
           {
+            System.out.printf("Command %d = %d : Pen Up, Turtle Ready To Move Without Colour\n", command, commands[command] );
             status = Status.UP;
-            System.out.println("Turtle Ready To Move Without Colouring Position\n");
           }
           break;
         case 2:
-          status = Status.DOWN;
-          System.out.println("Pen Down : Color Current Position");
+          {
+            System.out.printf("Command %d = %d : Pen Down, Turtle Ready To Colour\n", command, commands[command] );
+            status = Status.DOWN;
+
+            currentNow++;
+            currentNext++;
+            now = commands[currentNow];
+            next = commands[currentNext];
+          }
           break;
         case 3:
-          facing = Direction.RIGHT;
-          System.out.println("Turn Right : Ready To Increment currentY");
+          {
+            System.out.printf("Command %d = %d : Turtle Turns Right\n", command, commands[command] );
+            now = commands[command];
+            next = commands[command++];
+
+            currentDirection = determineDirection( facing, Turn.RIGHT );
+          }
           break;
+
         case 4:
-          facing = Direction.LEFT;
-          System.out.println("Turn Left : Ready To Decrement currentX");
+          {
+            System.out.printf("Command %d = %d : Turtle Turns Left\n", command, commands[command] );
+
+            currentDirection = determineDirection ( facing, Turn.LEFT );
+
+            currentNow++;
+            currentNext++;
+            now = commands[currentNow];
+            next = commands[currentNext];
+          }
           break;
         case 5:
           {
-            now = commands[command];
-            next = commands[command++];
-            System.out.printf("Move Forwards, Colouring %d Tiles\n", next );
-            if ( (facing == Direction.RIGHT) && (currentX + next <= 19) )
             {
-              for ( count = currentY; count < ( currentY + next ) % 20 ; count++)
+              now = commands[command];
+              next = commands[command + 1];
+
+              System.out.printf("Command %d = %d : Turtle Moves And Colours %d Tiles\n", command, commands[command], next );
+
+              currentDirection = determineDirection( facing, Turn.RIGHT );
+
+              switch ( currentDirection )
               {
-                floor[currentX][count] = 1;
+                case NORTH:
+                  {
+                    for ( count = ( currentX + next ) % 20; count >= currentX ; count--)
+                    {
+                      floor[count][currentY] = 1;
+                    }
+                  }
+                  break;
+                case WEST:
+                  {
+                    for ( count = currentY; count < ( currentY + next ) % 20 ; count++)
+                    {
+                      floor[currentX][count] = 1;
+                    }
+                  }
+                  break;
+                case SOUTH:
+                  {
+                    for ( count = currentX; count < ( currentX + next ) % 20 ; count++)
+                    {
+                      floor[count][currentY] = 1;
+                    }
+                  }
+                break;
+                case EAST:
+                  {
+                    for ( count = ( currentY + next ) % 20; count >= currentY ; count--)
+                    {
+                      floor[currentX][count] = 1;
+                    }
+                  }
+                  break;
               }
             }
 
-            else if ( (facing == Direction.RIGHT) && (currentX + next <= 19) )
-            {
-              for ( c = currentY; c < ( currentY + next ) % 20 ; c++)
-              {
-                floor[currentX][count] = 1;
-              }
-            }
+            currentNow++;
+            currentNext++;
+            now = commands[currentNow];
+            next = commands[currentNext];
           }
           break;
         case 6:
           {
-            System.out.println("Display The 20X20 Tiled Floor\n\n");
+            System.out.printf("Command %d = %d : Print The Tiled Floor\n", command, commands[command] );
 
             // Initialise floor array entries to 0
             for ( int i = 0; i < 20; i++ )
@@ -101,5 +157,46 @@ public class Turtle
           break;
       }
     }
+  }
+
+  public static Direction determineDirection ( Direction dir, Turn towards )
+  {
+    switch ( dir )
+    {
+      case NORTH:
+        {
+          if ( towards == Turn.LEFT )
+            dir = Direction.WEST;
+          else // Turn.RIGHT
+            dir = Direction.EAST;
+        }
+        break;
+      case WEST:
+        {
+          if ( towards == Turn.LEFT )
+            dir = Direction.SOUTH;
+          else // Turn.RIGHT
+            dir = Direction.NORTH;
+        }
+        break;
+      case SOUTH:
+        {
+          if ( towards == Turn.LEFT )
+            dir = Direction.EAST;
+          else // Turn.RIGHT
+            dir = Direction.WEST;
+        }
+        break;
+      case EAST:
+        {
+          if ( towards == Turn.LEFT )
+            dir = Direction.NORTH;
+          else // Turn.RIGHT
+            dir = Direction.SOUTH;
+        }
+        break;
+    }
+
+    return dir;
   }
 }
