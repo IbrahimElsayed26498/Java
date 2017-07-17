c855cc1baa74386e975685b190ffee20680d309b
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-07-17 11:21:54 +0100

    EmployeeTest Class Tests Composition

---
+++ Chapter8/Employee/EmployeeTest.java
@@ -0,0 +1,13 @@
+// EmployeeTest.java
+
+public class EmployeeTest
+{
+  public static void main( String[] args )
+  {
+    Date birth = new Date(7, 24, 1949);
+    Date hire = new Date(3, 12, 1988);
+    Employee employee = new Employee( "Bob", "Dylan", birth, hire );
+
+    System.out.println( employee );
+  }
+}
