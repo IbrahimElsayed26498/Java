82e40e6527a48e23bf6724a38a30c30f07f0f31a
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-07-20 14:34:02 +0100

    TrafficLightTest displays enum constants and their duration

---
+++ Chapter8/TrafficLight/TrafficLightTest.java
@@ -0,0 +1,17 @@
+// TrafficLightTest.java
+
+import java.util.EnumSet;
+
+public class TrafficLightTest
+{
+  public static void main( String[] args )
+  {
+    System.out.println("All TrafficLights: \n");
+
+    // print all trafficlights in enum light
+    for ( TrafficLight lights : TrafficLight.values() )
+        System.out.printf("%-10s%-45s\n", lights, lights.getDuration() );
+
+    System.out.println("\nDisplay A Range Of Enum Constants: \n");
+  }
+}
