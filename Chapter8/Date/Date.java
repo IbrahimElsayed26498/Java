05d3c110b65472854f45141992d5f628dd4c33c7
Isaac Kamga <isaac.kamga@ubuea.cm>
2017-07-22 09:37:30 +0100

    Exercise 8.14(a) Date Class

--- Chapter8/Date/Date.java
+++ Chapter8/Date/Date.java
@@ -76,4 +76,56 @@ public class Date
   {
     return String.format("%d/%d/%d", getMonth(), getDay(), getYear());
   }
+
+  public String toUnitedStatesString()
+  {
+    return String.format("%s %d, %d", monthToString(getMonth()), getDay(), getYear());
+  }
+
+  public String monthToString( int m)
+  {
+    String Month = "";
+
+    switch( m )
+    {
+      case 1:
+        Month = "January";
+        break;
+      case 2:
+        Month = "February";
+        break;
+      case 3:
+        Month = "March";
+        break;
+      case 4:
+        Month = "April";
+        break;
+      case 5:
+        Month = "May";
+        break;
+      case 6:
+        Month = "June";
+        break;
+      case 7:
+        Month = "July";
+        break;
+      case 8:
+        Month = "August";
+        break;
+      case 9:
+        Month = "September";
+        break;
+      case 10:
+        Month = "October";
+        break;
+      case 11:
+        Month = "November";
+        break;
+      case 12:
+        Month = "December";
+        break;
+    }
+
+    return Month;
+  }
 }
